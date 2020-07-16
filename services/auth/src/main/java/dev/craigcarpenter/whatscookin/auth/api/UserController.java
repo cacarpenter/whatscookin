82a7a84802c9fae32d928a35dee02f8a3a3228e2
Craig Carpenter <cacarpe@gmail.com>
2020-07-15 20:02:39 -0600

    Post user call

--- services/auth/src/main/java/dev/craigcarpenter/whatscookin/auth/api/UserController.java
+++ services/auth/src/main/java/dev/craigcarpenter/whatscookin/auth/api/UserController.java
@@ -1,15 +1,22 @@
 package dev.craigcarpenter.whatscookin.auth.api;
 
 import dev.craigcarpenter.whatscookin.auth.model.WCUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/v1")
 public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
-    @PostMapping
-    public void postUser(@Validated WCUser user) {
-
+    @PostMapping(value = "/users")
+    public void postUser(@Validated @RequestBody WCUser user) {
+      log.info("postUser");
+      log.info(user.getName());
     }
 }
--- services/auth/src/main/java/dev/craigcarpenter/whatscookin/auth/model/WCUser.java
+++ services/auth/src/main/java/dev/craigcarpenter/whatscookin/auth/model/WCUser.java
@@ -1,4 +1,24 @@
 package dev.craigcarpenter.whatscookin.auth.model;
 
 public class WCUser {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public WCUser setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public WCUser setName(String name) {
+        this.name = name;
+        return this;
+    }
 }
